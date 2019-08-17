8be53d59e5d694e53c853fca66fd541d781d0431
Evgeniy Devyatkin <mail@edevyatkin.com>
2019-08-17 17:05:40 +0300

    Add project files.

---
+++ LinkedList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CustomLinkedList
+{
+    public class LinkedList<T> : IEnumerable<T>
+    {
+
+        internal Node<T> Head { get; set; }
+
+        public LinkedList()
+        {
+
+        }
+
+        public LinkedList(params T[] values) => Array.ForEach(values, v => Insert(v));
+
+        private Node<T> Insert(Node<T> node)
+        {
+            node.Next = Head;
+            Head = node;
+            return Head;
+        }
+
+        public Node<T> Insert(T value) => Insert(new Node<T>(value));
+
+        public void Reverse()
+        {
+            if (Head == null)
+                return;
+            if (Head.Next == null)
+                return;
+
+            Node<T> prev = null;
+            Node<T> current = Head;
+
+            do {
+                var next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            } while (current != null);
+
+            Head = prev;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = Head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public class Node<T>
+        {
+            public T Value { get; set; }
+            public Node<T> Next { get; set; }
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CustomLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.Insert(5);
+            list.Insert(10);
+            list.Insert(4);
+            list.Insert(-1);
+            list.Insert(23);
+            list.Dump();
+            list.Reverse();
+            list.Dump();
+
+            Console.WriteLine();
+
+            var list2 = new LinkedList<int>(10, 55, 1, -42, 104, 28, 1024, -51);
+            list2.Dump();
+            list2.Reverse();
+            list2.Dump();
+        }
+    }
+
+    public static class LinkedListExtensions
+    {
+        public static void Dump<T>(this LinkedList<T> list)
+        {
+            Console.WriteLine(string.Join(" -> ", list));
+        }
+    }
+}
